78d46535b8512087b1c3b97356f0cdfc32a2d0b3
mattyh867 <matthewhughes.867@gmail.com>
2026-03-08 15:47:16 +0000

    Set up router files

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.database import engine
 from app.models.models import Base
+from app.routers import books, genres, reviews, authors
+
 
 Base.metadata.create_all(bind=engine)
 
@@ -9,6 +11,11 @@ app = FastAPI(
     description="A RESTful API for book metadata and analytics",
     version="1.0.0"
 )
+
+app.include_router(books.router, prefix="/books", tags=["Books"])
+app.include_router(genres.router, prefix="/genres", tags=["Genres"])
+app.include_router(reviews.router, prefix="/reviews", tags=["Reviews"])
+app.include_router(authors.router, prefix="/authors", tags=["Authors"])
 
 @app.get("/")
 def root():
